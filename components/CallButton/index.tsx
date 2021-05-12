fc4a1bc5827c442473447ad97f4530546d725981
Serghey <samsomskiff451@gmail.com>
2021-05-12 12:57:20 +0300

    add call btn anomation repeatadly

--- components/CallButton/index.tsx
+++ components/CallButton/index.tsx
@@ -1,4 +1,5 @@
 import Link from "next/link";
+import { useEffect, useRef } from "react";
 
 import styles from "./CallButton.module.scss";
 
@@ -7,10 +8,27 @@ interface CallButtonProps {
 }
 
 export default function CallButton({ tel }: CallButtonProps) {
+  const callBtn = useRef<HTMLSpanElement>(null);
+
+  function callBtnShaking() {
+    if (callBtn?.current !== null) {
+      callBtn.current.classList.toggle(`${styles.animation}`);
+    }
+  }
+
+  useEffect(() => {
+
+    const interval = setInterval(callBtnShaking, 5000)
+    
+    return () => {
+      clearInterval(interval)
+    }
+  }, []);
+
   return (
     <Link href={`tel:${tel}`}>
-      <a className="">
-        <span className={styles["phone-btn"]}>
+      <a>
+        <span ref={callBtn} className={styles["phone-btn"]}>
           <img
             className={styles["phone-img"]}
             src="/assets/contacts-img/phone-btn.svg"
