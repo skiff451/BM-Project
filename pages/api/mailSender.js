eca65c13edd18908f1504248a0d45265be3c98ce
Serghey <samsomskiff451@gmail.com>
2021-06-01 00:55:31 +0300

    trying to add env variable

--- pages/api/mailSender.js
+++ pages/api/mailSender.js
@@ -1,48 +1,45 @@
-import nodemailer from "nodemailer"
-
+import nodemailer from "nodemailer";
 
 export default async (req, res) => {
-    const { name, tel, email, comment } = req.body
+  const { name, tel, email, comment } = req.body;
 
-    const transporter = nodemailer.createTransport(
-        {
-            host: "smtp.meta.ua",
-            port: 465,
-            secureConnection: 'false',
-            tls: {
-                ciphers: 'SSLv3',
-                rejectUnauthorized: false
-            },
-            auth: {
-                user: "sbkursant@meta.ua",
-                pass: "08071989"
-            }
-        }
-    )
+  const transporter = nodemailer.createTransport({
+    host: "smtp.meta.ua",
+    port: 465,
+    secureConnection: "false",
+    tls: {
+      ciphers: "SSLv3",
+      rejectUnauthorized: false,
+    },
+    auth: {
+      user: "metalcut-client@meta.ua",
+      pass: process.env.MAIL_PASS,
+    },
+  });
 
-    // transporter.verify(function (error, success) {
-    //     if (error) {
-    //         console.log("Some problem.....!", error);
-    //     } else {
-    //         console.log("Server is ready to take our messages");
-    //     }
-    // });
+//   transporter.verify(function (error, success) {
+//     if (error) {
+//       console.log("Some problem.....!", error);
+//     } else {
+//       console.log("Server is ready to take our messages");
+//     }
+//   });
 
-    let info = await transporter.sendMail({
-        from: "sbkursant@meta.ua",
-        to: "samsonskiff@gmail.com",
-        subject: `New message from customer`,
-        text: `клиент: ${name}`,
-        html: `<h1>${name}</h1>
+  let info = await transporter.sendMail({
+    from: "metalcut-client@meta.ua",
+    to: "samsonskiff@gmail.com",
+    subject: `New message from customer`,
+    text: `клиент: ${name}`,
+    html: `<h1>${name}</h1>
                 <h2>${tel}</h2>
                 <p>${comment}
                 <br/>
                 ${email}
-                </p>`
-    });
-    if (info.response.split(' ')[0] === "250") {
-        res.status(200).send("ok");
-    } else {
-        res.status(400).send("failed");
-    }
-}
+                </p>`,
+  });
+  if (info.response.split(" ")[0] === "250") {
+    res.status(200).send("ok");
+  } else {
+    res.status(400).send("failed");
+  }
+};
