2e244bb0a11494b3756bb7a7ca00a01562b2bb95
Serghey <samsomskiff451@gmail.com>
2021-07-01 00:06:55 +0300

    add google analitics

--- components/CustomHead/index.tsx
+++ components/CustomHead/index.tsx
@@ -10,10 +10,7 @@ function CustomHead() {
         content="width=device-width, initial-scale=1, shrink-to-fit=no, user-scalable=no"
       ></meta>
       <meta charSet="utf-8" />
-      <meta
-        name="google-site-verification"
-        content="XNAMKWWPJim8izDd5WMFx9uWq70xK66WAEo3-7unlUY"
-      />
+
       <meta
         name="keywords"
         content="станки, станки плазменной резки, станки лазерной резки, 
@@ -30,15 +27,29 @@ function CustomHead() {
     "
       />
       <link rel="apple-touch-icon" href="/apple-ico.png"></link>
-      {/* <script>
-    window.ga=window.ga||function(){(ga.q = ga.q || []).push(arguments)}
-    ;ga.l=+new Date; ga('create', 'UA-XXXXX-Y', 'auto'); ga('send',
-    'pageview');
-  </script> */}
+
+      <meta
+        name="google-site-verification"
+        content="XNAMKWWPJim8izDd5WMFx9uWq70xK66WAEo3-7unlUY"
+      />
+
+      {/* Global Site Tag (gtag.js) - Google Analytics */}
       <script
         async
-        src="https://www.google-analytics.com/analytics.js"
-      ></script>
+        src={`https://www.googletagmanager.com/gtag/js?id=G-53YX31FT8L`}
+      />
+      <script
+        dangerouslySetInnerHTML={{
+          __html: `
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', 'G-53YX31FT8L', {
+              page_path: window.location.pathname,
+            });
+          `,
+        }}
+      />
     </Head>
   );
 }
